e8599a88d921a40786b769a65493ebdc938c2a5c
Partha Debnath <85936227+IM-Partha@users.noreply.github.com>
2025-09-02 23:00:17 +0530

    mongoose update

--- Backend/Modules/Database.js
+++ Backend/Modules/Database.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 
 mongoose.connect(process.env.MONGO_URL,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
   } ).then(()=>{
   console.log("MongoDb connect")
 }).catch((E)=>{
